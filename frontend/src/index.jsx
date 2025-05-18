7ceee8e4614ac63ba6bb8604328a7f144971e523
Katerina Chernova <chkaterina1@gmail.com>
2025-05-18 15:44:31 +0400

    Step 11: Add Rollbar error monitoring to React app

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,33 +1,45 @@
-import React from 'react';  
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';           
-import store from './app/store';                 
+import { Provider } from 'react-redux';
+import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
+import Rollbar from 'rollbar';
+
+import store from './app/store';
 import App from './App.jsx';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import './i18n';
 
-// Настройка словаря фильтрации
 import leoProfanity from 'leo-profanity';
 
-const dictionary = leoProfanity.getDictionary('en'); // английский
-const ruDictionary = leoProfanity.getDictionary('ru'); // русский
-
+const dictionary = leoProfanity.getDictionary('en');
+const ruDictionary = leoProfanity.getDictionary('ru');
 leoProfanity.add(dictionary);
 leoProfanity.add(ruDictionary);
 
 window.leoProfanity = leoProfanity;
 
+const rollbarConfig = {
+  accessToken: 'dc1a89b1fa0042b59cc489fb8b716e64',
+  environment: 'testenv',
+};
+
+const rollbar = new Rollbar(rollbarConfig);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>                       
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>,
+    <RollbarProvider instance={rollbar}>
+      <ErrorBoundary>
+        <Provider store={store}>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </Provider>
+      </ErrorBoundary>
+    </RollbarProvider>
+  </React.StrictMode>
 );
